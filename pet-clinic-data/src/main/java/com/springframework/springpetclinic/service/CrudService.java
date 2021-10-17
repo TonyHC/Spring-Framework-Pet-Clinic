e2b71a4ae3bd80d4b7515fca9aff65b98d7b52ff
Tony Chao <tchao@mail.usf.edu>
2021-10-17 16:22:47 -0400

    Refactoring to Common CRUD Interface. Closes #8

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/service/CrudService.java
@@ -0,0 +1,12 @@
+package com.springframework.springpetclinic.service;
+
+import java.util.Set;
+
+// Service Interface to mimic the Spring Repository
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+    T findById(ID id);
+    T save(T object);
+    void delete(T object);
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/service/VetService.java
@@ -2,10 +2,6 @@ package com.springframework.springpetclinic.service;
 
 import com.springframework.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
