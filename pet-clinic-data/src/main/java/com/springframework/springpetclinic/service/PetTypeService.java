f030403755e159117a6654b360f6363ab0d1185e
Tony Chao <tchao@mail.usf.edu>
2021-10-19 16:33:35 -0400

    Added PetTypeService and Implementation. Closes #26

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.springframework.springpetclinic.service;
+
+import com.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
