3293ba0ac6d2a43bd554df6c2037784f7551dd04
Tony Chao <tchao@mail.usf.edu>
2021-10-19 19:23:44 -0400

    Added Specialities Map Service and Implementation. Closes #27

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.springframework.springpetclinic.service;
+
+import com.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
