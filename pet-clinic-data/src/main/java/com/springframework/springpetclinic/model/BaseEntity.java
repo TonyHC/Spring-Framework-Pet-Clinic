5f269ad86c4cdbcab939d30f3a3e19452be91660
Tony Chao <tchao@mail.usf.edu>
2021-10-20 19:26:43 -0400

    Update Base Entity with @MappedSuperclass. Closes #32

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package com.springframework.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// Designate a Class whose mapping info is applied to Entities that inherit from it
+// Mapped Super Class has no separate Table defined for it
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
