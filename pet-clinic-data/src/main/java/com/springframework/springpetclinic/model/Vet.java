a2da34638e63916a6096cd9e83d97470b83ece47
Tony Chao <tchao@mail.usf.edu>
2021-10-20 20:42:05 -0400

    Convert Vets and Associatons to JPA Entities. Closes #36

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Vet.java
@@ -1,9 +1,19 @@
 package com.springframework.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    // Many To Many Uni-Directional Relationship: Many Vets have Many Specialities
+    // Use Eager Fetching to load all Vets and its Specialities
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+        joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id")
+    )
     private Set<Speciality> specialities;
 
     public Vet() {
