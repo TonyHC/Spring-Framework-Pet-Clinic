0a28b72fab5d4c6c301418e5808b7b3049a60d47
Tony Chao <tchao@mail.usf.edu>
2021-10-24 22:45:58 -0400

    Display Owner Record. Closes #51

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controller/OwnerController.java
@@ -4,7 +4,10 @@ import com.springframework.springpetclinic.model.Owner;
 import com.springframework.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -29,5 +32,12 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("/owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
