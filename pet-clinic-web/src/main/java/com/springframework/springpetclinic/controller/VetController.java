7974698ebb489be72a20762b34a07837f0048e1d
Tony Chao <tchao@mail.usf.edu>
2021-10-27 17:30:45 -0400

    Implement the Vets JSON Endpoint. Closes #59

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controller/VetController.java
@@ -4,7 +4,9 @@ import com.springframework.springpetclinic.model.Vet;
 import com.springframework.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
@@ -16,12 +18,19 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/vets", "/vets/list", "/vets/list.html", "/vets.html"})
+    @RequestMapping({"/vets", "/vets/list", "/vets/vetList.html", "/vets.html"})
     public String listVets(Model model) {
         Set<Vet> vets = vetService.findAll();
 
         model.addAttribute("vets", vets);
 
         return "vets/vetList";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        // Behind the scenes, Spring uses Jackson to convert the Vet Java POJO into JSON
+        // and display the JSON on /api/vets mapping
+        return vetService.findAll();
     }
 }
